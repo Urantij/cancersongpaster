f875f0508d782f25e9732cd3a8b012a15e93e8bc
Urantij <maximumuranti@gmail.com>
2024-12-07 05:33:02 +0700

    Изменит работу с буфером обмена

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,26 +1,52 @@
-use std::io;
-use std::io::Write;
-use std::process::{Command, Stdio};
+use arboard::Clipboard;
+use std::sync::{Arc, LazyLock, Mutex};
 
-pub fn paste_clipboard(content: &str) -> Result<(), io::Error> {
-    paste_xclip(content)
+struct ClipboardWrapper {
+    clipboard: Arc<Mutex<Clipboard>>,
 }
 
-fn paste_xclip(content: &str) -> Result<(), io::Error> {
-    let mut command = Command::new("xclip")
-        .args(["-sel", "clip"])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn()?;
+impl ClipboardWrapper {
+    fn new() -> ClipboardWrapper {
+        ClipboardWrapper {
+            clipboard: Arc::new(Mutex::new(Clipboard::new().unwrap())),
+        }
+    }
 
-    command
-        .stdin
-        .take()
-        .unwrap()
-        .write_all(content.as_bytes())?;
-
-    command.wait()?;
-
-    Ok(())
+    fn paste(&self, content: &str) -> Result<(), arboard::Error> {
+        self.clipboard.lock().unwrap().set_text(content)
+    }
 }
+
+static AR_WRAPPER: LazyLock<ClipboardWrapper> = LazyLock::new(|| ClipboardWrapper::new());
+
+pub fn paste_clipboard(content: &str) -> Result<(), arboard::Error> {
+    // paste_xclip(content)
+    paste_arboard(content)
+}
+
+fn paste_arboard(content: &str) -> Result<(), arboard::Error> {
+    // Если так делать, он почему-то вместо текста вставляет коды символов из текста
+    // let mut clipboard = Clipboard::new()?;
+    // clipboard.set_text(content)?;
+
+    AR_WRAPPER.paste(content)
+}
+
+// fn paste_xclip(content: &str) -> Result<(), io::Error> {
+//     let mut command = Command::new("xclip")
+//         .args(["-sel", "clip"])
+//         .stdin(Stdio::piped())
+//         .stdout(Stdio::null())
+//         .stderr(Stdio::null())
+//         .spawn()?;
+//
+//     command
+//         .stdin
+//         .take()
+//         .unwrap()
+//         .write_all(content.as_bytes())?;
+//
+//     command.wait()?;
+//
+//     Ok(())
+// }
